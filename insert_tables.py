47d38971a25d5774ef68e2d0cc2a19747d9d1aa2
Julia-Vaulina <109590167+Julia-Vaulina@users.noreply.github.com>
2022-12-06 22:54:51 +0200

    update code

--- insert_tables.py
+++ insert_tables.py
@@ -8,107 +8,81 @@ config = configparser.ConfigParser()
 config.read('token.ini')
 passw = config['BD_password']['passw']
 
+connection = psycopg2.connect(
+    user="postgres",
+    password=passw,
+    database="vkinder2022")
+
+connection.autocommit = True
+
 
 def insert_searchers(user_id):
+    """Заполнение таблицы searchers (пользователи, написавшие боту)"""
     try:
-        with psycopg2.connect(
-                database="vkinder2022",
-                user="postgres",
-                password=passw) as conn:
-            with conn.cursor() as cur:
-                cur.execute(
+        with connection.cursor() as cursor:
+            cursor.execute(
                     f"""INSERT INTO searchers (vk_id) 
                     VALUES ({user_id})
-                    ON CONFLICT (vk_id) DO NOTHING;"""
-                )
-            conn.commit()
-        conn.close()
+                    ON CONFLICT (vk_id) DO NOTHING;""")
     except Exception as error:
         print(error)  # Можно залогировать ошибки
         return
 
 
 def insert_requests(user_id):
+    """Заполнение таблицы requests (дата и время обращения к боту)"""
     try:
-        with psycopg2.connect(
-                database="vkinder2022",
-                user="postgres",
-                password=passw
-        ) as conn:
-            with conn.cursor() as cur:
-                cur.execute(
+        with connection.cursor() as cursor:
+            cursor.execute(
                     """INSERT INTO requests (date_time, searcher_id) 
-                    VALUES (%s, (SELECT searcherID from searchers WHERE vk_id=%s));
+                            VALUES (%s, (SELECT searcherID from searchers WHERE vk_id=%s));
                 """, (dt.today(), user_id))
-            conn.commit()
-        conn.close()
     except Exception as error:
         print(error)  # Можно залогировать ошибки
         return
 
 
 def insert_likes(user_id, result):
+    """Заполнение таблицы likes (сохраняем в БД данные страницы, которую пользователь добавил в "Избранное")"""
     try:
-        with psycopg2.connect(
-                database="vkinder2022",
-                user="postgres",
-                password=passw
-        ) as conn:
-            with conn.cursor() as cur:
-                cur.execute(
+        with connection.cursor() as cursor:
+            cursor.execute(
                     """INSERT INTO likes 
                     (searcher_id, user_vkid, name, surname, link) 
                     VALUES ((SELECT searcherID from searchers WHERE vk_id=%s),%s, %s, %s, %s)
                     ON CONFLICT (user_vkid) DO NOTHING;
-        """, (user_id, int(
-                        ''.join(
-                            re.findall(r'(\d+)', result['url'])
-                        )),
-                        result['name'].split()[0],
-                        result['name'].split()[1],
-                        result['url']))
-            conn.commit()
-        conn.close()
+        """, (user_id, int(''.join(re.findall(r'(\d+)', result['url']))),
+                result['name'].split()[0],
+                result['name'].split()[1],
+                result['url']))
     except Exception as error:
         print(error)  # Можно залогировать ошибки
         return
 
 
 def insert_dislikes(user_id, result):
+    """Заполнение таблицы dislikes (сохраняем в БД данные страницы, которую пользователь добавил в "черный список")"""
     try:
-        with psycopg2.connect(
-                database="vkinder2022",
-                user="postgres",
-                password=passw) as conn:
-            with conn.cursor() as cur:
-                cur.execute(
+        with connection.cursor() as cursor:
+            cursor.execute(
                     """INSERT INTO dislikes (searcher_id, user_vkid) 
                     VALUES ((SELECT searcherID from searchers WHERE vk_id=%s),%s);
-        """, (user_id, int(''.join(re.findall(r'(\d+)', result['url'])))))
-            conn.commit()
-        conn.close()
+                     """, (user_id, int(''.join(re.findall(r'(\d+)', result['url'])))))
     except Exception as error:
         print(error)  # Можно залогировать ошибки
         return
 
 
 def select_likes(user_id):
+    """Выбираем для пользователя все страницы, которые он отметил, как Like"""
     try:
-
-        with psycopg2.connect(
-                database="vkinder2022",
-                user="postgres",
-                password=passw) as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                        f"""SELECT l.name, l.surname, l.link 
+        with connection.cursor() as cursor:
+            cursor.execute(f"""SELECT l.name, l.surname, l.link 
                             FROM likes l
                             JOIN  searchers s 
                             ON l.searcher_id = s.searcherid  
-                            WHERE s.vk_id = {user_id};"""
-                )
+                            WHERE s.vk_id = {user_id};""")
                 result = [" ".join(fav) for fav in cur.fetchall()]
-                conn.close()
                 return result
     except Exception as error:
         print(error)  # Можно залогировать ошибки
